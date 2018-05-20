85e34539c6cf893f03ba0bac2742cd6fde4800a2
rgtodd <github@richtodd.com>
2018-05-20 17:39:12 -0500

    Docker / POM Updates

--- sfms/sfms.docker/src/main/java/hello/Application.java
+++ sfms/sfms.docker/src/main/java/hello/Application.java
@@ -1,21 +1,48 @@
 package hello;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+
 @SpringBootApplication
 @RestController
 public class Application {
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
-    }
+	private final Logger logger = Logger.getLogger(Application.class.getName());
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World - Version 2";
+	}
+
+	@RequestMapping("/test")
+	public String test() {
+
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		Query<Entity> query = Query.newEntityQueryBuilder().setKind("Ship").build();
+
+		int count = 0;
+		QueryResults<Entity> entities = datastore.run(query);
+		while (entities.hasNext()) {
+			Entity entity = entities.next();
+			logger.info(entity.getKey().toString());
+			count += 1;
+		}
+
+		return String.valueOf(count) + " records processed.";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
